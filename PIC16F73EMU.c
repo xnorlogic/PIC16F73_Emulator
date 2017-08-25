8f3cb5caa6d62cf943af77fa9691e49dfc1985fd
Andres R <xnorlogic@gmail.com>
2017-08-25 12:01:57 -0500

    Update PIC16F73EMU.c

--- PIC16F73EMU.c
+++ PIC16F73EMU.c
@@ -1,16 +1,9 @@
 #include "PIC16F73.h"
-#include "ALU.h"
+#include "InstructionDecode.h"
 
 PIC16F73 Emulator;
 
-int main()
-{
-	//file location of PIC16F73. 7 bits to access any memory location
-	Emulator.f = 0x7F;
-	
-	//Memory allocation to emulate PIC16F73
-	Memory_Allocation(&Emulator);
-	
+void Registe_Status_Proveout(){
 	//Registe Status Prove out------------------------------------------------------
 	//Bank Select-------------------------------------------------------------------
 	Emulator.RP0 = 0;
@@ -22,7 +15,6 @@ int main()
 	Emulator.Z   = 0; //Zero bit
 	Emulator.DC  = 0; //digit carry
 	Emulator.C   = 0; //carry
-	
 	//write Status Register
 	REG_STATUS_WRT(&Emulator);
 	//Display status register
@@ -32,14 +24,36 @@ int main()
 	//Allocate Status Register in its appropiate PIC16F73 memory address
 	Emulator.Data_Memory[Data_Memory_Address(&Emulator)] = REG_STATUS_RD(&Emulator);
 	//------------------------------------------------------------------------------
+}
+
+int main()
+{	
+	//Test Program----------------------------------------------------
+	//Load Program into program memory
+	Emulator.Program_Memory[0x0005] = 0x300a; 	//MOVLW 0x01
+	Emulator.Program_Memory[0x0006] = 0x00A1; 	//MOVWF 0x21
+	Emulator.Program_Memory[0x0007] = 0x07A1; 	//ADDWF 0x21, f
+	Emulator.PC = 0x0005;			        //Send program
+							//counter to the 0x05
+							//program memory start
+	//----------------------------------------------------------------
 	
-	//initialize a specific memory location using the file location
-	//Emulator.Data_Memory[Bank_Select(&Emulator) + Emulator.f] = (byte)0x00;
+	for (int CNT=0;CNT<3;CNT++){
 	
-	printf("PIC16F73 BankSelect      = %x\n",Bank_Select(&Emulator));
-	printf("PIC16F73 Memory location = %x\n",Data_Memory_Address(&Emulator));
-	printf("Host Memory location     = %x\n",&Emulator.Data_Memory[Data_Memory_Address(&Emulator)]);
-	printf("value                    = %x\n",Emulator.Data_Memory[Data_Memory_Address(&Emulator)]);
+		Emulator.OPCODE = (Emulator.Program_Memory[Emulator.PC] >> 8) & 255;
+		Emulator.d 		= (Emulator.Program_Memory[Emulator.PC] >> 7) & 1;
+		Emulator.f 		= Emulator.Program_Memory[Emulator.PC] & 127;
+		Emulator.k 		= Emulator.Program_Memory[Emulator.PC] & 255;
+		
+		Emulator.PC = Instruction_Decode(&Emulator, Emulator.OPCODE, Emulator.PC);
+		
+		printf("\n");
+		printf("f   = 0x%02x\n",Emulator.Data_Memory[Data_Memory_Address(&Emulator)]);
+		printf("W   = 0x%02x\n",Emulator.W);
+		printf("C   = %x\n",Emulator.C);
+		printf("DC  = %x\n",Emulator.DC);
+		printf("Z   = %x\n",Emulator.Z);
+	}
 	
 	//Free memory for cleanup
 	Memory_Free(&Emulator);

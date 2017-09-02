cf1ab2f11599fe87fc3f5bdad6b3217b7d17b98b
xnorlogic <xnorlogic@gmail.com>
2017-09-02 13:44:12 -0500

    build into DLL

--- compilelink.bat
+++ compilelink.bat
@@ -4,4 +4,5 @@ echo Get ready!...
 g++ -c -o PIC16F73.o PIC16F73.c
 g++ -c -o PIC16F73EMU.o PIC16F73EMU.c
 g++ -o PicEmulator PIC16F73.o PIC16F73EMU.o
+g++ -shared -o PIC16F73.dll PIC16F73.o
 PicEmulator.exe

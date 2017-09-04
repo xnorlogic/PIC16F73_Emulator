fa0eb6b354c2e73b7cacb6adb869e30195e43cd3
Andres R <xnorlogic@gmail.com>
2017-09-04 13:55:09 -0500

    Update PIC16F73.c

--- PIC16F73.c
+++ PIC16F73.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "PIC16F73.h"
+#include "ALU.h"
 
 Emulator PIC16F73;
 Special_Function_Register STATUS;
@@ -90,23 +91,6 @@ void Memory_Free(Emulator *PIC16F7x){
 	free(PIC16F7x->Data_Memory);
 	free(PIC16F7x->Program_Memory);
 	free(PIC16F7x->Program_Physical_Memory);
-}
-
-byte ADD(Special_Function_Register *Gen_Use_Register, word W1, word W2){
-
-	word W3;
-	W3 = W1 + W2;
-	
-	if(((W3 >> 8) & 1) == 1) Gen_Use_Register->BIT_0 = 1;
-	else Gen_Use_Register->BIT_0 = 0;
-	
-	if(((W3 >> 4) & 1) == 1) Gen_Use_Register->BIT_1 = 1;
-	else Gen_Use_Register->BIT_1 = 0;
-	
-	if(W3 == 0) Gen_Use_Register->BIT_2 = 1;
-	else Gen_Use_Register->BIT_2 = 0;
-
-	return W3;
 }
 
 word Instruction_Decode (Emulator *PIC16F7x, byte OPCODE, word ProgCNT){

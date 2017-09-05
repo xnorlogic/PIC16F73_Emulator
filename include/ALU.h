aa85586ec2ee9e76f63218744198deebacc649aa
Andres R <xnorlogic@gmail.com>
2017-09-05 13:36:47 -0500

    Create ALU.h

---
+++ include/ALU.h
@@ -0,0 +1,10 @@
+#ifndef _ALU_H
+//ALU*****************************************************************************************
+byte ADD(Special_Function_Register*, word, word);
+//byte SUB(Special_Function_Register*, word, word);
+/*
+ALU prototypes!!
+
+*/
+//********************************************************************************************
+#endif

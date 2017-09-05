ae359d491ff8acbe827c7282ce2ac69d872ded91
Andres R <xnorlogic@gmail.com>
2017-09-05 13:39:56 -0500

    Delete compilelink.bat

--- compilelink.bat
+++
@@ -1,9 +0,0 @@
-echo off
-cls
-echo Get ready!...
-g++ -c -o PIC16F73.o PIC16F73.c
-g++ -c -o ALU.o ALU.c
-g++ -c -o PIC16F73EMU.o PIC16F73EMU.c
-
-g++ -o PicEmulator PIC16F73.o ALU.o PIC16F73EMU.o
-g++ -shared -o PIC16F73.dll PIC16F73.o ALU.o

cbb18f30541bd52c423ad36711b6cae04a5eb4b3
Andres R <xnorlogic@gmail.com>
2017-09-06 11:05:46 -0500

    Update ALU.h
    added a few more prototypes

--- include/ALU.h
+++ include/ALU.h
@@ -1,8 +1,8 @@
 //ALU*****************************************************************************************
 byte ADD(Special_Function_Register*, word, word);
-//byte SUB(Special_Function_Register*, word, word);
-/*
-ALU prototypes!!
-
-*/
-//********************************************************************************************
+byte AND(Special_Function_Register *Gen_Use_Register, word W1, word W2);
+byte IOR(Special_Function_Register *Gen_Use_Register, word W1, word W2);
+byte XOR(Special_Function_Register *Gen_Use_Register, word W1, word W2);
+byte DEC(Special_Function_Register *Gen_Use_Register, word W1);
+byte INC(Special_Function_Register *Gen_Use_Register, word W1);
+byte COMP(Special_Function_Register *Gen_Use_Register, word W1);

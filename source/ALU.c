c54a5223316435d98616e4828762f78970a51a6a
Andres R <xnorlogic@gmail.com>
2017-09-06 11:06:37 -0500

    Update ALU.c

--- source/ALU.c
+++ source/ALU.c
@@ -19,3 +19,69 @@ byte ADD(Special_Function_Register *Gen_Use_Register, word W1, word W2){
 
 	return W3;
 }
+
+byte AND(Special_Function_Register *Gen_Use_Register, word W1, word W2){
+
+	word W3;
+	W3 = W1 & W2;
+
+	if(W3 == 0) Gen_Use_Register->BIT_2 = 1;
+	else Gen_Use_Register->BIT_2 = 0;
+
+	return W3;
+}
+
+byte IOR(Special_Function_Register *Gen_Use_Register, word W1, word W2){
+
+	word W3;
+	W3 = W1 | W2;
+
+	if(W3 == 0) Gen_Use_Register->BIT_2 = 1;
+	else Gen_Use_Register->BIT_2 = 0;
+
+	return W3;
+}
+
+byte XOR(Special_Function_Register *Gen_Use_Register, word W1, word W2){
+
+	word W3;
+	W3 = W1 ^ W2;
+
+	if(W3 == 0) Gen_Use_Register->BIT_2 = 1;
+	else Gen_Use_Register->BIT_2 = 0;
+
+	return W3;
+}
+
+byte DEC(Special_Function_Register *Gen_Use_Register, word W1){
+	
+	word W3;
+	W3 = W1 - 0x01;
+	
+	if(W3 == 0) Gen_Use_Register->BIT_2 = 1;
+	else Gen_Use_Register->BIT_2 = 0;
+	
+	return W3;
+}
+
+byte INC(Special_Function_Register *Gen_Use_Register, word W1){
+
+	word W3;
+	W3 = W1 + 0x01;
+	
+	if(W3 == 0) Gen_Use_Register->BIT_2 = 1;
+	else Gen_Use_Register->BIT_2 = 0;
+	
+	return W3;
+}
+
+byte COMP(Special_Function_Register *Gen_Use_Register, word W1){
+
+	word W3;
+	W3 = ~ W1;
+	
+	if(W3 == 0) Gen_Use_Register->BIT_2 = 1;
+	else Gen_Use_Register->BIT_2 = 0;
+	
+	return W3;
+}
